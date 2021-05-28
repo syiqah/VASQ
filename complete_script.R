b89e27abe5aa51fb88fa8d3a9cc62a1e6aebd96d
syiqah <asyiqah.2020@mitb.smu.edu.sg>
2021-05-28 20:46:16 +0800

    first commit

---
+++ complete_script.R
@@ -0,0 +1,16 @@
+# Starting Git
+install.packages("usethis")
+usethis::use_git()
+
+# Configuring and Pushing to Github
+usethis::use_github()
+#Error: Can't determine the default branch for this repo
+#Do you need to make your first commit?
+
+# Configure Git
+#Tools -> Shell to open the Git Shell
+#git config
+#git config
+# Run usethis::use_github() again
+
+
